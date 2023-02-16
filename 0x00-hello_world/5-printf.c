b481830384ac4d82a5d6025ce9ad36a8234e7104
Suedreamer <mulingesuzan88@gmail.com>
2023-02-16 12:26:37 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+include<stdioh.h>
+/**
+* main - A program that prints a line using printf function
+* return 0 (success)
+*/
+int main(void)
+{
+	printf("with proper,but87 the outcomeis a piece of art,\n")
+		return (0)
+}
