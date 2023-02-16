66dbea6fe8b725ffa97710444451c3a473c0383c
Suedreamer <mulingesuzan88@gmail.com>
2023-02-16 11:56:41 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+include<stdioh.h>
+/**
+ main -Entry point
+ *
+ return: always 0 (success)
+*/
+int main(void)
+{
+	puts("\"programming is like bulding a multilingual puzzle");
+	
+	return (0);
+}	
