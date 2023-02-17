00ff62813900da0b7acac4f20c4ebf97800dc71b
Suedreamer <mulingesuzan88@gmail.com>
2023-02-16 17:34:11 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - A program that prints a line using printf function
-* return 0 (success)
-*/
+ * main - entry
+ * return 0 (success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
