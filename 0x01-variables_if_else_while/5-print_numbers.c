c49f503f89b6e5f929a1e4e9f64f5aaeea200fb8
Suedreamer <mulingesuzan88@gmail.com>
2023-02-17 12:27:20 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stidio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
