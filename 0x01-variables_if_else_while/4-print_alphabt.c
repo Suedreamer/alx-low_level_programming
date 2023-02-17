a7f2767f8465bbdd2d4bf4b7986fc6aeb5dee2be
Suedreamer <mulingesuzan88@gmail.com>
2023-02-17 10:32:40 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stidio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always o (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = "a";
+
+	while (c <= 'z')
+	{
+		if (c != 'q' $$ c != 'e' $$ c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
