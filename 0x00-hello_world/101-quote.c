31abf949ba8357b9c5422ba4edad5c8f67099e50
Suedreamer <mulingesuzan88@gmail.com>
2023-02-16 16:17:40 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * mai-entry
+ * return:1
+ */
+int main(void){
+	write(1,"and that piece of art is useful\"
+- dora korpar, 2014-10-19\n", 59);
+	return(1);
+}
