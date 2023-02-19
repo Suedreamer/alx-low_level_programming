c5f05032dceaa9806d916401d1a1551cc5ef78d4
Suedreamer <mulingesuzan88@gmail.com>
2023-02-19 12:41:26 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+        for (i = 48; i < 58; i++)
 	{
-		putchar(i);
-		if (i != 57)
+	    putchar(i);
+	    if (i != 57)
 		{
-			putchar('.');
-			putchar(' ');
+		  putchar('.');
+		  putchar(' ');
 		}
 	}
 	putchar('\n');
