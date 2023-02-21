8b456904df5bfbc7935d1f05f8a56ba294ca810e
Suedreamer <mulingesuzan88@gmail.com>
2023-02-21 09:38:08 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,20 +3,18 @@
 
 int _putchar(char);
 void print_alphabet(void);
-void print _alphabet_x10(void);
-int _islower(int c);
+void print_alphabet_x10(void);
+int _islower (int);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
-int print_last_night(int);
+int _ abs (int);
+int print _last_digit(int);
 void jack_bauer(void);
+int add(int, int);
 void times_table(void);
-int add(int,int);
 void print_to_98(int n);
-void print_times_table(int n)
+void print_times_table(int n);
 
+#endif
 
-endif
-}
-	
 
