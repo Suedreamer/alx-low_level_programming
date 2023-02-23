d81a7660e7a707df3a0416b8a9a7e87b37720d85
Suedreamer <mulingesuzan88@gmail.com>
2023-02-22 23:53:29 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+int _putchar(char c);
+int _isuper(int c);
+int _isdigit (int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+
+#endif.
