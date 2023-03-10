eec1cded3a8380f55d22c30e587eea1d6319cd22
Suedreamer <mulingesuzan88@gmail.com>
2023-03-10 09:16:11 -0800

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
+
+# Compile all C source files in current directory into object files
+gcc -c *.c
+
+# Create a static library called liball.a from the object files
+ar rc liball.a *.o
+
+# Index the library for faster linking
 ranlib liball.a
+
+# Remove the object files
+rm *.o
+
