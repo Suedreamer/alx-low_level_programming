c99e300247a76fa5c4641e9a943c923ed84f2a76
Suedreamer <mulingesuzan88@gmail.com>
2023-03-30 03:10:06 -0700

    0-print_lists.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _LISTS_
+#define _LISTS_
 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
