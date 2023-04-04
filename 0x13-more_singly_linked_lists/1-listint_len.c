87133f3cccbc9fec4ec3b9075164eba24f1de87a
Suedreamer <mulingesuzan88@gmail.com>
2023-04-04 01:18:30 -0700

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,10 +11,11 @@ size_t listint_len(const listint_t *h)
 {
 	size_t nnodes = 0;
 
-	while (n != NULL)
+	while (h != NULL)
 	{
-		n = n->next;
+		h = h->next;
 		nnodes++;
 	}
 	return (nnodes);
 }
+
